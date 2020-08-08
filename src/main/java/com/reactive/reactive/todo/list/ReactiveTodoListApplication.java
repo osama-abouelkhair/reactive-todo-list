217d1bdce3c2608161b9e11f2bcd19716817b40a
osama abo elkhair <osama.abulkhair@gmail.com>
2020-08-08 18:31:31 +0200

    Setup maven dependencies

---
+++ src/main/java/com/reactive/reactive/todo/list/ReactiveTodoListApplication.java
@@ -0,0 +1,13 @@
+package com.reactive.reactive.todo.list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveTodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveTodoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reactive/reactive/todo/list/ReactiveTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactive.reactive.todo.list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveTodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
