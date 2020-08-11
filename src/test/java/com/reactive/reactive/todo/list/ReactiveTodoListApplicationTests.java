1c6c39eb984988fb1526cb4e23d6bd2efa648972
osama abo elkhair <osama.abulkhair@gmail.com>
2020-08-11 23:42:07 +0200

    Setup and COnfigure test for web flux

--- src/test/java/com/reactive/reactive/todo/list/ReactiveTodoListApplicationTests.java
+++ src/test/java/com/reactive/reactive/todo/list/ReactiveTodoListApplicationTests.java
@@ -1,13 +1,37 @@
 package com.reactive.reactive.todo.list;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.http.HttpHeaders;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.UUID;
 
 @SpringBootTest
 class ReactiveTodoListApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    ApplicationContext context;
+
+    WebTestClient rest;
+
+    @BeforeEach
+    public void init() {
+        this.rest = WebTestClient
+                .bindToApplicationContext(this.context)
+                .configureClient()
+                .build();
+    }
+
+    @Test
+    void contextLoads() {
+        rest.get().uri("/lists/{id}", UUID.randomUUID())
+                .header(HttpHeaders.ACCEPT, "application/json")
+                .exchange()
+                .expectStatus().isOk();
+    }
 
 }
